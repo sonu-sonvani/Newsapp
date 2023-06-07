098240e328658974291724c43da00b7d86917ca3
sonu-sonvani <134940889+sonu-sonvani@users.noreply.github.com>
2023-06-07 11:52:32 +0530

    Update App.js
    no change

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ export default class App extends Component {
     
       />
     <Navbar/>
-//         this is navbar
+       
     {/* <News  category="general"/> */}
     <Routes>
           {/* <Route path="/" key="general"><News  category="general"/></Route> */}
